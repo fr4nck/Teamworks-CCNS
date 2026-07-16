fdb290065693bee5c4ccba707bb98d1ef14e67d6
fr4nck <f.bellardie@gmail.com>
2026-07-16 13:46:11 +0200

    Ajoute le socle métier des responsabilités

---
+++ domain/access/__init__.py
@@ -0,0 +1,7 @@
+"""Objets métier d'accès, de responsabilités et d'espaces de travail."""
+
+from .responsibility import Responsibility
+from .role import Role
+from .workspace import Workspace
+
+__all__ = ["Responsibility", "Role", "Workspace"]
