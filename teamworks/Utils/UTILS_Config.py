25978c35baef7a7c3dae470861ba724e51242ee8
fr4nck <f.bellardie@gmail.com>
2026-07-25 13:14:34 +0200

    Frankenstein lot 2 — restaurer l’API complète de configuration

--- teamworks/Utils/UTILS_Config.py
+++ teamworks/Utils/UTILS_Config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-15 -*-
+# -*- coding: utf-8 -*-
 #------------------------------------------------------------------------
 # Application :    Noethys, gestion multi-activités
 # Site internet :  www.noethys.com
@@ -91,3 +91,106 @@ class FichierConfig():
         data = self.GetDictConfig()
         data[key] = valeur
         self.SetDictConfig(data)
+
+    def SetItemsConfig(self, dictParametres={}):
+        """ Remplace plusieurs valeur dans le fichier de config """
+        """ dictParametres = {nom : valeur, nom : valeur...} """
+        data = self.GetDictConfig()
+        for key, valeur in list(dictParametres.items()):
+            data[key] = valeur
+        self.SetDictConfig(data)
+
+    def DelItemConfig(self, key ):
+        """ Supprime une valeur dans le fichier de config """
+        data = self.GetDictConfig()
+        del data[key]
+        self.SetDictConfig(data)
+
+
+
+def GetParametre(nomParametre="", defaut=None):
+    parametre = None
+    try :
+        topWindow = wx.GetApp().GetTopWindow()
+        nomWindow = topWindow.GetName()
+    except :
+        nomWindow = None
+    if nomWindow == "general" : 
+        # Si la frame 'General' est chargée, on y récupère le dict de config
+        if nomParametre in topWindow.userConfig :
+            parametre = topWindow.userConfig[nomParametre]
+        else :
+            parametre = defaut
+    else:
+        # Récupération du nom de la DB directement dans le fichier de config sur le disque dur
+        cfg = FichierConfig()
+        parametre = cfg.GetItemConfig(nomParametre, defaut)
+    return parametre
+
+def SetParametre(nomParametre="", parametre=None):
+    try :
+        topWindow = wx.GetApp().GetTopWindow()
+        nomWindow = topWindow.GetName()
+    except :
+        nomWindow = None
+    if nomWindow == "general" : 
+        # Si la frame 'General' est chargée, on y récupère le dict de config
+        topWindow.userConfig[nomParametre] = parametre
+    else:
+        # Enregistrement du nom de la DB directement dans le fichier de config sur le disque dur
+        cfg = FichierConfig()
+        cfg.SetItemConfig(nomParametre, parametre)
+
+
+
+# --------------Traitement par lot ------------------------------------------------------------------------------------------
+
+def GetParametres(dictParametres={}):
+    """ dictParametres = {nom : valeur, nom: valeur...} """
+    dictFinal = {}
+    try :
+        topWindow = wx.GetApp().GetTopWindow()
+        nomWindow = topWindow.GetName()
+    except :
+        nomWindow = None
+        
+    # Cherche la sources des données
+    if nomWindow == "general" : 
+        dictSource = topWindow.userConfig
+    else :
+        cfg = FichierConfig()
+        dictSource = cfg.GetDictConfig()
+        
+    # Lit les données
+    for nom, valeur in list(dictParametres.items()) :
+        if nom in dictSource :
+            dictFinal[nom] = dictSource[nom]
+        else :
+            dictFinal[nom] = valeur
+    return dictFinal
+
+
+
+def SetParametres(dictParametres={}):
+    """ dictParametres = {nom : valeur, nom: valeur...} """
+    try :
+        topWindow = wx.GetApp().GetTopWindow()
+        nomWindow = topWindow.GetName()
+    except :
+        nomWindow = None
+    if nomWindow == "general" : 
+        # Si la frame 'General' est chargée, on y récupère le dict de config
+        for nom, valeur in list(dictParametres.items()) :
+            topWindow.userConfig[nom] = valeur
+    else:
+        # Enregistrement dans le fichier de config sur le disque dur
+        cfg = FichierConfig()
+        cfg.SetItemsConfig(dictParametres)
+    return dictParametres
+
+
+
+# --------------- TESTS ----------------------------------------------------------------------------------------------------------
+if __name__ == u"__main__":
+    print(("GET :", GetParametres( {"impression_factures_impayes" : 0} )))
+    #print("SET :", SetParametres( {"test1" : True, "test2" : True} ))
