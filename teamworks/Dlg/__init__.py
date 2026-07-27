377815b8c4309f9fd424e6745905971369d7a5e6
fr4nck <f.bellardie@gmail.com>
2026-07-27 21:50:38 +0200

    TW-068 install targeted refresh after problem patch

--- teamworks/Dlg/__init__.py
+++ teamworks/Dlg/__init__.py
@@ -11,7 +11,9 @@ def __getattr__(name):
     module = importlib.import_module("%s.DLG_Fiche_individuelle" % __name__)
     lazy = importlib.import_module("%s.DLG_Fiche_individuelle_lazy" % __name__)
     problems = importlib.import_module("%s.DLG_Fiche_individuelle_problems" % __name__)
+    refresh = importlib.import_module("%s.DLG_Fiche_individuelle_refresh" % __name__)
     lazy.install(module)
     problems.install(module)
+    refresh.install(module)
     globals()[name] = module
     return module
