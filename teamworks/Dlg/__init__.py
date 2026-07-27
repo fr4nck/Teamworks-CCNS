e6d7a0245010210f4e0f8479957a1e70f861e6c0
fr4nck <f.bellardie@gmail.com>
2026-07-27 21:21:35 +0200

    TW-066: activer la fiche individuelle différée à l’import

---
+++ teamworks/Dlg/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Chargement ciblé des dialogues historiques."""
+
+import importlib
+
+
+def __getattr__(name):
+    if name != "DLG_Fiche_individuelle":
+        raise AttributeError(name)
+
+    module = importlib.import_module("%s.DLG_Fiche_individuelle" % __name__)
+    lazy = importlib.import_module("%s.DLG_Fiche_individuelle_lazy" % __name__)
+    lazy.install(module)
+    globals()[name] = module
+    return module
