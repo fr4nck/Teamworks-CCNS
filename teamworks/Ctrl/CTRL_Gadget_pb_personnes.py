b8525d88c89d0bdf13921592a557c3c4dabd08dc
fr4nck <f.bellardie@gmail.com>
2026-08-20 00:18:09 +0200

    TW-186 — Dossiers incomplets : parler de documents, pas de contrats (#256)

--- teamworks/Ctrl/CTRL_Gadget_pb_personnes.py
+++ teamworks/Ctrl/CTRL_Gadget_pb_personnes.py
@@ -17,7 +17,7 @@ class Panel(wx.Panel):
     def __init__(self, parent, ID=-1):
         wx.Panel.__init__(self, parent, ID, name="panel_gadget_pb_personnes", style=wx.TAB_TRAVERSAL)
         self.barreTitre = FonctionsPerso.BarreTitre(self,  _(u"Problèmes des fiches"), u"")
-        self.treeCtrl = TreeCtrl(self)    
+        self.treeCtrl = TreeCtrl(self)
 
         # Layout
         grid_sizer_principal = wx.FlexGridSizer(rows=3, cols=1, vgap=0, hgap=0)
@@ -33,7 +33,7 @@ class PanelGadget(wx.Panel):
     """ Version spéciale pour Gadget Page d'acccueil """
     def __init__(self, parent, ID=-1):
         wx.Panel.__init__(self, parent, ID, name="panel_gadget_pb_personnes", style=wx.TAB_TRAVERSAL)
-        self.treeCtrl = TreeCtrl(self)    
+        self.treeCtrl = TreeCtrl(self)
 
         # Layout
         grid_sizer_principal = wx.FlexGridSizer(rows=1, cols=1, vgap=0, hgap=0)
@@ -42,8 +42,8 @@ class PanelGadget(wx.Panel):
         grid_sizer_principal.Fit(self)
         grid_sizer_principal.AddGrowableRow(0)
         grid_sizer_principal.AddGrowableCol(0)
-        
-        
+
+
 class MyFrame(wx.Frame):
     def __init__(self, parent, title="" ):
         wx.Frame.__init__(self, parent, -1, title=title, name="frm_gadget_pb_personnes", style=wx.DEFAULT_FRAME_STYLE)
@@ -66,39 +66,39 @@ class TreeCtrl(CT.CustomTreeCtrl):
     def __init__(self, parent, fichier="", id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.NO_BORDER) :
         CT.CustomTreeCtrl.__init__(self, parent, id, pos, size, style)
         self.parent = parent
-        
+
         # Diminution de la taille de la police sous linux
         if "linux" in sys.platform :
             defaultFont = self.GetFont()
             defaultFont.SetPointSize(8)
             self.SetFont(defaultFont)
-        
+
         self.SetAGWWindowStyleFlag(wx.TR_HIDE_ROOT | wx.TR_HAS_BUTTONS | wx.TR_HAS_VARIABLE_ROW_HEIGHT | CT.TR_AUTO_CHECK_CHILD)
-        self.EnableSelectionVista(True) 
-        
+        self.EnableSelectionVista(True)
+
         # Couleurs
         self.couleurFond = (122, 161, 230)
         self.couleurPersonne = (255, 255, 255)
         self.couleurType = (0, 0, 0)
         self.couleurProbleme = (0, 0, 0)
         self.couleurTraits = (179, 185, 231)
-        
+
         # Autres paramètres
-        if self.parent.GetName() != "panel_gadget_dossiersincomplets" : 
+        if self.parent.GetName() != "panel_gadget_dossiersincomplets" :
             self.expandPersonnes = True
             self.expandTypes = True
         else:
             self.expandPersonnes = False
             self.expandTypes = False
-        
+
         # Bind
         self.Bind(wx.EVT_LEAVE_WINDOW, self.OnLeftDown)
 
-        
+
     def OnLeftDown(self, event):
         self.Unselect()
         event.Skip()
-        
+
     def MAJ_treeCtrl(self):
         self.DeleteAllItems()
         # Init couleurs
@@ -127,7 +127,7 @@ class TreeCtrl(CT.CustomTreeCtrl):
                 self.SetItemTextColour(newItem, self.couleurProbleme)
             else:
                 # Tête de rubrique
-                texte = item[0]                
+                texte = item[0]
                 newItem = self.AppendItem(parentItem, texte)
                 self.SetItemData(newItem, None)
                 if parentItem == self.root :
@@ -139,13 +139,23 @@ class TreeCtrl(CT.CustomTreeCtrl):
                     # Type de problème
                     self.SetItemTextColour(newItem, self.couleurType)
                     # self.SetItemBold(newItem, True)
-                    if self.expandPersonnes : self.Expand(parentItem) 
-                
+                    if self.expandPersonnes : self.Expand(parentItem)
+
                 # Autres
                 self.AddTreeNodes(newItem, item[1], img)
-                if self.expandTypes : self.Expand(newItem) 
+                if self.expandTypes : self.Expand(newItem)
 
+    def _GetCategoryLabel(self, nomCategorie, valeurs):
+        """Corrige le vocabulaire historique du groupe contrat/DUE.
 
+        Le compteur historique additionne des documents/actions à traiter
+        (contrat non signé, DUE à faire, etc.) et non des contrats distincts.
+        """
+        if nomCategorie == _(u"1 contrat à voir"):
+            return _(u"1 document à traiter")
+        if nomCategorie.endswith(_(u" contrats à voir")):
+            return str(len(valeurs)) + _(u" documents à traiter")
+        return nomCategorie
 
     def GetListeProblemes(self):
         dictNoms, dictProblemes = FonctionsPerso.Creation_liste_pb_personnes()
@@ -156,11 +166,11 @@ class TreeCtrl(CT.CustomTreeCtrl):
             if IDpersonne in dictNoms:
                 listeProblemes.append( [dictNoms[IDpersonne], []] )
                 for nomCategorie, valeurs in dictCategories.items() :
+                    nomCategorie = self._GetCategoryLabel(nomCategorie, valeurs)
                     listeProblemes[index1][1].append( [nomCategorie, valeurs] )
                 index1 += 1
         return listeProblemes
-           
-            
+
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 
