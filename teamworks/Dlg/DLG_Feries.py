25e47a1e60fe54f199e9ed6d667b12b6e557f2e0
fr4nck <f.bellardie@gmail.com>
2026-08-21 07:26:46 +0200

    TW-189 — Moderniser la gestion des jours fériés

--- teamworks/Dlg/DLG_Feries.py
+++ teamworks/Dlg/DLG_Feries.py
@@ -8,9 +8,9 @@
 # Licence:         Licence GNU GPL
 #------------------------------------------------------------------------
 
-
 import Chemins
 from Utils import UTILS_Adaptations
+from Utils import UTILS_Interface
 from Utils.UTILS_Traduction import _
 import wx
 from Ctrl import CTRL_Bouton_image
@@ -18,59 +18,79 @@ from Ctrl import CTRL_Bandeau
 from Ol import OL_Feries
 
 
+def _dip(window, width, height):
+    """Convertit une taille logique en pixels en respectant le DPI natif."""
+    try:
+        return window.FromDIP(wx.Size(width, height))
+    except Exception:
+        return wx.Size(width, height)
+
+
+def _section_title(parent, label):
+    ctrl = wx.StaticText(parent, -1, label)
+    font = ctrl.GetFont()
+    font.SetWeight(wx.FONTWEIGHT_BOLD)
+    font.SetPointSize(max(10, font.GetPointSize() + 1))
+    ctrl.SetFont(font)
+    ctrl.SetForegroundColour(UTILS_Interface.GetToken("on_surface"))
+    return ctrl
+
 
 class Panel(wx.Panel):
     def __init__(self, parent, type="fixe"):
         wx.Panel.__init__(self, parent, id=-1, name="panel_feries", style=wx.TAB_TRAVERSAL)
         self.parent = parent
         self.type = type
-        
-        self.staticbox = wx.StaticBox(self, -1, _(u"Jours %ss") % self.type)
-        self.ctrl_listview = OL_Feries.ListView(self, type=self.type, id=-1, style=wx.LC_REPORT|wx.SUNKEN_BORDER|wx.LC_SINGLE_SEL|wx.LC_HRULES|wx.LC_VRULES)
+        self.SetBackgroundColour(UTILS_Interface.GetToken("surface_container_lowest"))
+
+        self.label_titre = _section_title(self, _(u"Jours %ss") % self.type)
+        self.ctrl_listview = OL_Feries.ListView(
+            self,
+            type=self.type,
+            id=-1,
+            style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES | wx.LC_VRULES,
+        )
+        self.ctrl_listview.SetBackgroundColour(UTILS_Interface.GetToken("surface_container_lowest"))
         self.ctrl_listview.MAJ()
         self.ctrl_recherche = OL_Feries.CTRL_Outils(self, listview=self.ctrl_listview)
 
-        self.bouton_ajouter = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Ajouter.png"), wx.BITMAP_TYPE_ANY))
-        self.bouton_modifier = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Modifier.png"), wx.BITMAP_TYPE_ANY))
-        self.bouton_supprimer = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Supprimer.png"), wx.BITMAP_TYPE_ANY))
-        
-        # Propriétés
+        self.bouton_ajouter = CTRL_Bouton_image.CTRL(
+            self, texte=_(u"Ajouter"), cheminImage="Images/32x32/Ajouter.png"
+        )
+        self.bouton_modifier = CTRL_Bouton_image.CTRL(
+            self, texte=_(u"Modifier"), cheminImage="Images/32x32/Modifier.png"
+        )
+        self.bouton_supprimer = CTRL_Bouton_image.CTRL(
+            self, texte=_(u"Supprimer"), cheminImage="Images/32x32/Supprimer.png"
+        )
+
         self.bouton_ajouter.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour ajouter un jour férié %s") % self.type))
         self.bouton_modifier.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour modifier le jour férié %s sélectionné dans la liste") % self.type))
         self.bouton_supprimer.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour supprimer le jour férié %s sélectionné dans la liste") % self.type))
-        
-        # Layout
-        sizer_staticbox = wx.StaticBoxSizer(self.staticbox, wx.VERTICAL)
-        
-        grid_sizer_contenu = wx.FlexGridSizer(rows=1, cols=2, vgap=5, hgap=5)
-        
-        grid_sizer_gauche = wx.FlexGridSizer(rows=3, cols=1, vgap=10, hgap=10)
-        grid_sizer_gauche.Add(self.ctrl_listview, 0, wx.EXPAND, 0)
-        grid_sizer_gauche.Add(self.ctrl_recherche, 0, wx.EXPAND, 0)
-        grid_sizer_gauche.AddGrowableRow(0)
-        grid_sizer_gauche.AddGrowableCol(0)
-        grid_sizer_contenu.Add(grid_sizer_gauche, 1, wx.EXPAND, 0)
-        
-        grid_sizer_droit = wx.FlexGridSizer(rows=4, cols=1, vgap=5, hgap=5)
-        grid_sizer_droit.Add(self.bouton_ajouter, 0, 0, 0)
-        grid_sizer_droit.Add(self.bouton_modifier, 0, 0, 0)
-        grid_sizer_droit.Add(self.bouton_supprimer, 0, 0, 0)
-        grid_sizer_contenu.Add(grid_sizer_droit, 1, wx.EXPAND, 0)
-        grid_sizer_contenu.AddGrowableRow(0)
-        grid_sizer_contenu.AddGrowableCol(0)
-        
-        sizer_staticbox.Add(grid_sizer_contenu, 1, wx.ALL|wx.EXPAND, 5)
-        
-        self.SetSizer(sizer_staticbox)
-        
-        # Binds
+
+        self.__do_layout()
+
         self.Bind(wx.EVT_BUTTON, self.Ajouter, self.bouton_ajouter)
         self.Bind(wx.EVT_BUTTON, self.Modifier, self.bouton_modifier)
         self.Bind(wx.EVT_BUTTON, self.Supprimer, self.bouton_supprimer)
 
+    def __do_layout(self):
+        sizer_base = wx.BoxSizer(wx.VERTICAL)
+        sizer_base.Add(self.label_titre, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 10)
+        sizer_base.Add(self.ctrl_listview, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 10)
+        sizer_base.Add(self.ctrl_recherche, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 10)
+
+        sizer_actions = wx.WrapSizer(wx.HORIZONTAL)
+        sizer_actions.Add(self.bouton_ajouter, 0, wx.RIGHT | wx.BOTTOM, 6)
+        sizer_actions.Add(self.bouton_modifier, 0, wx.RIGHT | wx.BOTTOM, 6)
+        sizer_actions.Add(self.bouton_supprimer, 0, wx.RIGHT | wx.BOTTOM, 6)
+        sizer_base.Add(sizer_actions, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 10)
+
+        self.SetSizer(sizer_base)
+
     def Ajouter(self, event):
         self.ctrl_listview.Ajouter(None)
-        
+
     def Modifier(self, event):
         self.ctrl_listview.Modifier(None)
 
@@ -78,72 +98,88 @@ class Panel(wx.Panel):
         self.ctrl_listview.Supprimer(None)
 
 
-
-
-
 class Dialog(wx.Dialog):
     def __init__(self, parent):
-        wx.Dialog.__init__(self, parent, -1, style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MAXIMIZE_BOX|wx.MINIMIZE_BOX)
+        wx.Dialog.__init__(
+            self,
+            parent,
+            -1,
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER | wx.MAXIMIZE_BOX | wx.MINIMIZE_BOX,
+        )
         self.parent = parent
-        
+        self.SetBackgroundColour(UTILS_Interface.GetToken("surface"))
+
         intro = _(u"Vous pouvez ici saisir, modifier ou supprimer des jours fériés. Ces informations sont utilisées dans le calendrier de saisie des consommations et dans le paramétrage des unités des activités.")
         titre = _(u"Gestion des jours fériés")
-        self.ctrl_bandeau = CTRL_Bandeau.Bandeau(self, titre=titre, texte=intro, hauteurHtml=30, nomImage=Chemins.GetStaticPath("Images/32x32/Jour.png"))
+        self.ctrl_bandeau = CTRL_Bandeau.Bandeau(
+            self,
+            titre=titre,
+            texte=intro,
+            hauteurHtml=30,
+            nomImage=Chemins.GetStaticPath("Images/32x32/Jour.png"),
+        )
         self.panel_variables = Panel(self, type="variable")
         self.panel_fixes = Panel(self, type="fixe")
-        
-        self.bouton_aide = CTRL_Bouton_image.CTRL(self, texte=_(u"Aide"), cheminImage="Images/32x32/Aide.png")
-        self.bouton_saisie_auto = CTRL_Bouton_image.CTRL(self, texte=_(u"Génération auto. des jours variables"), cheminImage="Images/32x32/Magique.png")
-        self.bouton_fermer = CTRL_Bouton_image.CTRL(self, id=wx.ID_CANCEL, texte=_(u"Fermer"), cheminImage="Images/32x32/Fermer.png")
+
+        self.bouton_aide = CTRL_Bouton_image.CTRL(
+            self, texte=_(u"Aide"), cheminImage="Images/32x32/Aide.png"
+        )
+        self.bouton_saisie_auto = CTRL_Bouton_image.CTRL(
+            self,
+            texte=_(u"Génération auto. des jours variables"),
+            cheminImage="Images/32x32/Magique.png",
+        )
+        self.bouton_fermer = CTRL_Bouton_image.CTRL(
+            self,
+            id=wx.ID_CANCEL,
+            texte=_(u"Fermer"),
+            cheminImage="Images/32x32/Fermer.png",
+        )
 
         self.__set_properties()
         self.__do_layout()
-        
+
         self.Bind(wx.EVT_BUTTON, self.OnBoutonAide, self.bouton_aide)
         self.Bind(wx.EVT_BUTTON, self.OnBoutonSaisieAuto, self.bouton_saisie_auto)
-        
 
     def __set_properties(self):
         self.SetTitle(_(u"Gestion des jours fériés"))
         self.bouton_aide.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour obtenir de l'aide")))
         self.bouton_saisie_auto.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour saisir automatiquement les jours fériés variables")))
         self.bouton_fermer.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour fermer")))
-        self.SetMinSize((550, 680))
+        self.SetMinSize(_dip(self, 680, 620))
+        self.SetSize(_dip(self, 860, 760))
 
     def __do_layout(self):
-        grid_sizer_base = wx.FlexGridSizer(rows=4, cols=1, vgap=10, hgap=10)
-        grid_sizer_base.Add(self.ctrl_bandeau, 0, wx.EXPAND, 0)
-        
-        grid_sizer_base.Add(self.panel_variables, 1, wx.LEFT|wx.RIGHT|wx.EXPAND, 10)
-        grid_sizer_base.Add(self.panel_fixes, 1, wx.LEFT|wx.RIGHT|wx.EXPAND, 10)
-        
-        grid_sizer_boutons = wx.FlexGridSizer(rows=1, cols=4, vgap=10, hgap=10)
-        grid_sizer_boutons.Add(self.bouton_aide, 0, 0, 0)
-        grid_sizer_boutons.Add(self.bouton_saisie_auto, 0, 0, 0)
-        grid_sizer_boutons.Add((20, 20), 0, wx.EXPAND, 0)
-        grid_sizer_boutons.Add(self.bouton_fermer, 0, 0, 0)
-        grid_sizer_boutons.AddGrowableCol(2)
-        grid_sizer_base.Add(grid_sizer_boutons, 1, wx.LEFT|wx.RIGHT|wx.BOTTOM|wx.EXPAND, 10)
-        
-        self.SetSizer(grid_sizer_base)
-        grid_sizer_base.Fit(self)
-        grid_sizer_base.AddGrowableRow(1)
-        grid_sizer_base.AddGrowableRow(2)
-        grid_sizer_base.AddGrowableCol(0)
+        sizer_base = wx.BoxSizer(wx.VERTICAL)
+        sizer_base.Add(self.ctrl_bandeau, 0, wx.EXPAND)
+
+        sizer_contenu = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_contenu.Add(self.panel_variables, 1, wx.EXPAND | wx.RIGHT, 6)
+        sizer_contenu.Add(self.panel_fixes, 1, wx.EXPAND | wx.LEFT, 6)
+        sizer_base.Add(sizer_contenu, 1, wx.EXPAND | wx.ALL, 12)
+
+        sizer_boutons = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_boutons.Add(self.bouton_aide, 0, wx.RIGHT, 8)
+        sizer_boutons.Add(self.bouton_saisie_auto, 0, wx.RIGHT, 8)
+        sizer_boutons.AddStretchSpacer(1)
+        sizer_boutons.Add(self.bouton_fermer, 0)
+        sizer_base.Add(sizer_boutons, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, 12)
+
+        self.SetSizer(sizer_base)
         self.Layout()
         self.CenterOnScreen()
 
-    def OnBoutonAide(self, event): 
+    def OnBoutonAide(self, event):
         from Utils import UTILS_Aide
         UTILS_Aide.Aide("Joursfris")
 
     def OnBoutonSaisieAuto(self, event):
         from Dlg import DLG_Saisie_feries_auto
         dlg = DLG_Saisie_feries_auto.MyDialog(self)
-        if dlg.ShowModal() == wx.ID_OK :
+        if dlg.ShowModal() == wx.ID_OK:
             self.panel_variables.ctrl_listview.MAJ()
         dlg.Destroy()
-        
 
 
 if __name__ == "__main__":
