db4421816fe412d6daa9ad3259cd476387e12689
fr4nck <f.bellardie@gmail.com>
2026-08-24 18:54:46 +0200

    TW-182: corriger le parentage Phoenix de la fiche structure

--- teamworks/Dlg/DLG_Organisation.py
+++ teamworks/Dlg/DLG_Organisation.py
@@ -13,7 +13,12 @@ from Utils import UTILS_Organisation
 
 class Dialog(wx.Dialog):
     def __init__(self, parent):
-        super().__init__(parent, title="Structure / Association", size=(860, 700))
+        super().__init__(
+            parent,
+            title="Structure / Association",
+            size=(860, 700),
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
+        )
         self.profile = UTILS_Organisation.GetProfil()
         self.initial_logo_path = UTILS_Branding.GetAssociationLogoPath()
         self.remove_logo = False
@@ -71,43 +76,46 @@ class Dialog(wx.Dialog):
         main = wx.BoxSizer(wx.VERTICAL)
 
         identity_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Identité")
+        identity_parent = identity_box.GetStaticBox()
         grid = wx.FlexGridSizer(4, 2, 8, 12)
         grid.AddGrowableCol(1, 1)
-        self._row(page, grid, "Nom officiel :", "nom_officiel")
-        self._row(page, grid, "Nom d’usage :", "nom_usage")
-        self._row(page, grid, "Représentant légal :", "representant_legal")
-        self._row(page, grid, "Fonction :", "representant_fonction")
+        self._row(identity_parent, grid, "Nom officiel :", "nom_officiel")
+        self._row(identity_parent, grid, "Nom d’usage :", "nom_usage")
+        self._row(identity_parent, grid, "Représentant légal :", "representant_legal")
+        self._row(identity_parent, grid, "Fonction :", "representant_fonction")
         identity_box.Add(grid, 0, wx.EXPAND | wx.ALL, 10)
         main.Add(identity_box, 0, wx.EXPAND | wx.ALL, 10)
 
         contact_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Coordonnées")
+        contact_parent = contact_box.GetStaticBox()
         contact_grid = wx.FlexGridSizer(7, 2, 8, 12)
         contact_grid.AddGrowableCol(1, 1)
-        self._row(page, contact_grid, "Adresse :", "adresse")
-        self._row(page, contact_grid, "Code postal :", "code_postal")
-        self._row(page, contact_grid, "Ville :", "ville")
-        self._row(page, contact_grid, "Téléphone :", "telephone")
-        self._row(page, contact_grid, "Email :", "email")
-        self._row(page, contact_grid, "Site web :", "site_web")
+        self._row(contact_parent, contact_grid, "Adresse :", "adresse")
+        self._row(contact_parent, contact_grid, "Code postal :", "code_postal")
+        self._row(contact_parent, contact_grid, "Ville :", "ville")
+        self._row(contact_parent, contact_grid, "Téléphone :", "telephone")
+        self._row(contact_parent, contact_grid, "Email :", "email")
+        self._row(contact_parent, contact_grid, "Site web :", "site_web")
         contact_box.Add(contact_grid, 0, wx.EXPAND | wx.ALL, 10)
         main.Add(contact_box, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, 10)
 
         logo_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Logo")
+        logo_parent = logo_box.GetStaticBox()
         logo_row = wx.BoxSizer(wx.HORIZONTAL)
         self.logo_picker = wx.FilePickerCtrl(
-            page,
+            logo_parent,
             path=self.initial_logo_path,
             message="Sélectionner le logo de la structure",
             wildcard="Images (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp",
             style=wx.FLP_OPEN | wx.FLP_FILE_MUST_EXIST | wx.FLP_USE_TEXTCTRL,
         )
         logo_row.Add(self.logo_picker, 1, wx.EXPAND | wx.RIGHT, 8)
-        remove = wx.Button(page, label="Retirer")
+        remove = wx.Button(logo_parent, label="Retirer")
         logo_row.Add(remove, 0)
         logo_box.Add(logo_row, 0, wx.EXPAND | wx.ALL, 10)
         hint = wx.StaticText(
-            page,
-            label="PNG transparent recommandé. Ce logo sera réutilisé dans le splash, l’accueil et les documents.",
+            logo_parent,
+            label="PNG transparent recommandé. Ce logo sera réutilisé dans le splash et les documents.",
         )
         logo_box.Add(hint, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM, 10)
         main.Add(logo_box, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, 10)
@@ -122,31 +130,34 @@ class Dialog(wx.Dialog):
         main = wx.BoxSizer(wx.VERTICAL)
 
         legal_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Identifiants légaux")
+        legal_parent = legal_box.GetStaticBox()
         legal_grid = wx.FlexGridSizer(7, 2, 8, 12)
         legal_grid.AddGrowableCol(1, 1)
-        self._row(page, legal_grid, "RNA / n° association :", "rna")
-        self._row(page, legal_grid, "SIREN :", "siren")
-        self._row(page, legal_grid, "SIRET :", "siret")
-        self._row(page, legal_grid, "Code APE / NAF :", "ape_naf")
-        self._row(page, legal_grid, "Préfecture / déclaration :", "declaration_prefecture")
-        self._row(page, legal_grid, "Référence JOAFE :", "reference_joafe")
+        self._row(legal_parent, legal_grid, "RNA / n° association :", "rna")
+        self._row(legal_parent, legal_grid, "SIREN :", "siren")
+        self._row(legal_parent, legal_grid, "SIRET :", "siret")
+        self._row(legal_parent, legal_grid, "Code APE / NAF :", "ape_naf")
+        self._row(legal_parent, legal_grid, "Préfecture / déclaration :", "declaration_prefecture")
+        self._row(legal_parent, legal_grid, "Référence JOAFE :", "reference_joafe")
         legal_box.Add(legal_grid, 0, wx.EXPAND | wx.ALL, 10)
         main.Add(legal_box, 0, wx.EXPAND | wx.ALL, 10)
 
         approval_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Agrément")
+        approval_parent = approval_box.GetStaticBox()
         approval_grid = wx.FlexGridSizer(2, 2, 8, 12)
         approval_grid.AddGrowableCol(1, 1)
-        self._row(page, approval_grid, "Agrément Jeunesse et Sports / JEP :", "agrement_js")
-        self._row(page, approval_grid, "Date / validité :", "agrement_js_date")
+        self._row(approval_parent, approval_grid, "Agrément Jeunesse et Sports / JEP :", "agrement_js")
+        self._row(approval_parent, approval_grid, "Date / validité :", "agrement_js_date")
         approval_box.Add(approval_grid, 0, wx.EXPAND | wx.ALL, 10)
         main.Add(approval_box, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, 10)
 
         insurance_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Assurance")
+        insurance_parent = insurance_box.GetStaticBox()
         insurance_grid = wx.FlexGridSizer(3, 2, 8, 12)
         insurance_grid.AddGrowableCol(1, 1)
-        self._row(page, insurance_grid, "Assureur :", "assureur")
-        self._row(page, insurance_grid, "N° de police :", "police_assurance")
-        self._row(page, insurance_grid, "Échéance / validité :", "assurance_echeance")
+        self._row(insurance_parent, insurance_grid, "Assureur :", "assureur")
+        self._row(insurance_parent, insurance_grid, "N° de police :", "police_assurance")
+        self._row(insurance_parent, insurance_grid, "Échéance / validité :", "assurance_echeance")
         insurance_box.Add(insurance_grid, 0, wx.EXPAND | wx.ALL, 10)
         main.Add(insurance_box, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, 10)
 
@@ -176,15 +187,21 @@ class Dialog(wx.Dialog):
             ("afficher_assurance", "Afficher l’assurance / n° de police"),
         ]
         box = wx.StaticBoxSizer(wx.VERTICAL, page, "En-tête documentaire")
+        box_parent = box.GetStaticBox()
         for key, label in labels:
-            ctrl = wx.CheckBox(page, label=label)
+            ctrl = wx.CheckBox(box_parent, label=label)
             ctrl.SetValue(bool(self.profile.get(key, False)))
             self.flags[key] = ctrl
             box.Add(ctrl, 0, wx.ALL, 6)
         main.Add(box, 0, wx.EXPAND | wx.LEFT | wx.RIGHT, 12)
 
         preview_box = wx.StaticBoxSizer(wx.VERTICAL, page, "Aperçu textuel")
-        self.preview = wx.TextCtrl(page, style=wx.TE_MULTILINE | wx.TE_READONLY, size=(-1, 150))
+        preview_parent = preview_box.GetStaticBox()
+        self.preview = wx.TextCtrl(
+            preview_parent,
+            style=wx.TE_MULTILINE | wx.TE_READONLY,
+            size=(-1, 150),
+        )
         preview_box.Add(self.preview, 1, wx.EXPAND | wx.ALL, 8)
         main.Add(preview_box, 1, wx.EXPAND | wx.ALL, 12)
         refresh = wx.Button(page, label="Actualiser l’aperçu")
