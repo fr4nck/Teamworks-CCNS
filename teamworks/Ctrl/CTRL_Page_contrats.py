fd9bdea10b9c4fda6a9d9c6350ce724a5fc65e5a
fr4nck <f.bellardie@gmail.com>
2026-08-30 13:23:05 +0200

    Contrats: finaliser la validation de la suppression atomique

--- teamworks/Ctrl/CTRL_Page_contrats.py
+++ teamworks/Ctrl/CTRL_Page_contrats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Page contrats modernisée : cœur historique conservé, suppression atomique active."""
+"""Page contrats modernisée : cœur historique conservé et suppression atomique validée."""
 
 import wx
 
