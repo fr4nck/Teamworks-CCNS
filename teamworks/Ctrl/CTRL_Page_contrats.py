477fccd434d4a04d4f64a665df08984c78ce9aa2
fr4nck <f.bellardie@gmail.com>
2026-08-30 13:57:13 +0200

    Contrats: fiabiliser signature et DUE

--- teamworks/Ctrl/CTRL_Page_contrats.py
+++ teamworks/Ctrl/CTRL_Page_contrats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Page contrats modernisée : cœur historique conservé et suppression atomique validée."""
+"""Page contrats modernisée : cœur historique conservé et écritures critiques vérifiées."""
 
 import wx
 
@@ -9,7 +9,7 @@ from Ctrl.CTRL_Page_contrats_core import *  # Compatibilité des imports histori
 
 
 class Panel_Contrats(CORE.Panel_Contrats):
-    """Écran contrats actif avec suppression transactionnelle."""
+    """Écran contrats actif avec écritures transactionnelles contrôlées."""
 
     def SupprimerContrat(self):
         index = self.list_ctrl_contrats.GetFirstSelected()
@@ -74,3 +74,63 @@ class Panel_Contrats(CORE.Panel_Contrats):
         self.list_ctrl_contrats.Remplissage()
         self.MAJ_barre_problemes()
         return True
+
+    def _BasculerIndicateur(self, colonne, champ, libelle):
+        index = self.list_ctrl_contrats.GetFirstSelected()
+        if index == -1:
+            dlg = wx.MessageDialog(
+                self,
+                CORE._(u"Vous devez d'abord sélectionner un contrat dans la liste."),
+                "Information",
+                wx.OK | wx.ICON_INFORMATION,
+            )
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
+
+        IDcontrat = self.list_ctrl_contrats.GetItemData(index)
+        valeur_actuelle = self.list_ctrl_contrats.GetItem(index, colonne).GetText()
+        nouvelle_valeur = "" if valeur_actuelle == "Oui" else "Oui"
+
+        DB = CORE.GestionDB.DB()
+        placeholder = "%s" if DB.isNetwork else "?"
+        try:
+            DB.cursor.execute(
+                "UPDATE contrats SET %s=%s WHERE IDcontrat=%s" % (
+                    champ,
+                    placeholder,
+                    placeholder,
+                ),
+                (nouvelle_valeur, IDcontrat),
+            )
+            if DB.cursor.rowcount == 0:
+                raise RuntimeError(CORE._(u"Le contrat sélectionné n'existe plus."))
+            DB.Commit()
+        except Exception as err:
+            try:
+                DB.connexion.rollback()
+            except Exception:
+                pass
+            DB.Close()
+            wx.MessageBox(
+                CORE._(
+                    u"L'indicateur %s n'a pas pu être modifié. Aucune modification n'a été validée.\n\n"
+                    u"Détail technique : %s"
+                ) % (libelle, err),
+                CORE._(u"Modification annulée"),
+                wx.OK | wx.ICON_ERROR,
+                parent=self,
+            )
+            return False
+        DB.Close()
+
+        # L'interface ne reflète la nouvelle valeur qu'après validation en base.
+        self.list_ctrl_contrats.SetItem(index, colonne, nouvelle_valeur)
+        self.MAJ_barre_problemes()
+        return True
+
+    def OnBoutonSignature(self, event):
+        return self._BasculerIndicateur(4, "signature", CORE._(u"Signature"))
+
+    def OnBoutonDue(self, event):
+        return self._BasculerIndicateur(5, "due", CORE._(u"DUE"))
