c4f0b982d8cf149233b34297528ab4bf881a8aff
fr4nck <f.bellardie@gmail.com>
2026-09-03 13:20:26 +0200

    POC Qt: ajouter un lanceur Windows autonome

---
+++ poc/qt-theme/run_windows.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if not exist ".venv\Scripts\python.exe" (
+  echo [Teamworks Qt POC] Creation de l'environnement isole...
+  py -m venv .venv || goto :error
+)
+
+echo [Teamworks Qt POC] Mise a jour des dependances du POC...
+".venv\Scripts\python.exe" -m pip install -q --disable-pip-version-check -r requirements.txt || goto :error
+
+echo [Teamworks Qt POC] Lancement...
+".venv\Scripts\python.exe" app.py
+goto :eof
+
+:error
+echo.
+echo Echec du lancement du POC Qt.
+pause
+exit /b 1
