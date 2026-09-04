16d49c8cebfd12dc739a7974581cb85c68b5c706
fr4nck <f.bellardie@gmail.com>
2026-09-04 17:39:18 +0200

    POC Qt: exposer le lancement Recrutement

--- run_windows.cmd
+++ run_windows.cmd
@@ -1,4 +1,8 @@
 @echo off
 setlocal
-call "%~dp0poc\qt-theme\run_windows.cmd"
+if /I "%~1"=="recruitment" (
+  call "%~dp0poc\qt-theme\run_recruitment_windows.cmd"
+) else (
+  call "%~dp0poc\qt-theme\run_windows.cmd"
+)
 exit /b %ERRORLEVEL%
