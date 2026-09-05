336b37136537ed0b4a1d4788f43482bcacb73e7c
fr4nck <f.bellardie@gmail.com>
2026-09-05 08:14:32 +0200

    Qt: adapter les tests contrats à la date de rupture

--- tests/test_qt_contract_projection.py
+++ tests/test_qt_contract_projection.py
@@ -13,11 +13,12 @@ if str(QT_POC) not in sys.path:
 from production_read_adapter import TeamworksProductionReadAdapter
 
 
-def _contract(*, date_fin):
+def _contract(*, date_fin, date_rupture=None):
     return SimpleNamespace(
         type_contrat="CDI",
         date_debut="2020-09-01",
         date_fin=date_fin,
+        date_rupture=date_rupture,
         classification="Groupe 4",
         temps_hebdo=35.0,
     )
@@ -38,3 +39,11 @@ def test_contract_projection_keeps_real_fixed_end_date():
     )
 
     assert view.end == "31/12/2026"
+
+
+def test_contract_projection_prefers_historical_rupture_date():
+    view = TeamworksProductionReadAdapter._contract_to_view(
+        _contract(date_fin="2026-12-31", date_rupture="2025-11-30")
+    )
+
+    assert view.end == "30/11/2025-R"
