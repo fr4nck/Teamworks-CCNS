f732c5a3206e517ad2f9d277e9730fd0c959218d
PMSL35 <direction@pelemele.org>
2026-09-21 17:23:42 +0200

    refactor(wx): uniformiser les boutons d'action historiques

--- teamworks/Ctrl/CTRL_Calendrier.py
+++ teamworks/Ctrl/CTRL_Calendrier.py
@@ -11,6 +11,7 @@
 import Chemins
 from Utils.UTILS_Traduction import _
 import wx
+from Ctrl import CTRL_Bouton_image
 import six
 import wx.lib.newevent
 import calendar
@@ -978,7 +979,7 @@ class CTRL(wx.Panel):
         if afficheAujourdhui == True and self.selectionInterdite == False :
             self.calendrier.SelectJours( [datetime.date.today(),] )
             
-        self.bouton_CalendrierAnnuel = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath("Images/16x16/Calendrier_jour.png"), wx.BITMAP_TYPE_PNG), size=(28, 21))
+        self.bouton_CalendrierAnnuel = CTRL_Bouton_image.CTRL(self, id=-1, texte="", cheminImage=Chemins.GetStaticPath("Images/16x16/Calendrier_jour.png"))
         self.bouton_CalendrierAnnuel.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour afficher le calendrier annuel")))
         
         # Layout
