0c41d31df17c77e3c0c70d970a67f169d2084f62
PMSL35 <direction@pelemele.org>
2026-09-21 17:07:11 +0200

    fix(wx): conserver la largeur naturelle des boutons historiques

--- teamworks/Ctrl/CTRL_Calendrier.py
+++ teamworks/Ctrl/CTRL_Calendrier.py
@@ -984,7 +984,7 @@ class CTRL(wx.Panel):
         # Layout
         sizer =  wx.BoxSizer(wx.VERTICAL)
         sizerOptions = wx.FlexGridSizer(rows=1, cols=8, vgap=0, hgap=0)
-        sizerOptions.Add(self.bouton_CalendrierAnnuel, 0, wx.EXPAND|wx.RIGHT, 5)
+        sizerOptions.Add(self.bouton_CalendrierAnnuel, 0, wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
         sizerOptions.Add(self.combo_mois, 0, wx.EXPAND, 0)
         sizerOptions.Add(self.spin, 0, wx.EXPAND, 0)
         sizerOptions.Add( (5, 5), 0, wx.EXPAND, 0)
