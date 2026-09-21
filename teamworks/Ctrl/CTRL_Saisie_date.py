412c6df012b9f31c67668c3624c336eb5d09fa70
PMSL35 <direction@pelemele.org>
2026-09-21 17:43:21 +0200

    refactor(wx): migrer les sélecteurs compacts vers le contrôle commun

--- teamworks/Ctrl/CTRL_Saisie_date.py
+++ teamworks/Ctrl/CTRL_Saisie_date.py
@@ -11,6 +11,7 @@
 import Chemins
 from Utils import UTILS_Adaptations
 import wx
+from Ctrl import CTRL_Bouton_image
 import wx.lib.masked as masked
 import re
 import sys
@@ -400,7 +401,7 @@ class Date2(wx.Panel):
         self.heure = heure
         
         self.ctrl_date = Date(self, date_min, date_max)
-        self.bouton_calendrier = wx.BitmapButton(self, -1, wx.Bitmap(Chemins.GetStaticPath(u"Images/16x16/Calendrier.png"), wx.BITMAP_TYPE_ANY))  # native-button-ok: déclencheur compact intégré au champ date
+        self.bouton_calendrier = CTRL_Bouton_image.Compact(self, cheminImage=Chemins.GetStaticPath(u"Images/16x16/Calendrier.png"))
         self.Bind(wx.EVT_BUTTON, self.OnBoutonCalendrier, self.bouton_calendrier)
         self.bouton_calendrier.SetToolTip(wx.ToolTip(_(u"Cliquez ici pour sélectionner la date dans le calendrier")))
 
